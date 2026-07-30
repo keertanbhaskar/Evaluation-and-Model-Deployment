2792c3d53c1e468edcd2233573b45a03fce6e25f
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-07-30 23:53:58 +0530

    Instead of slider ,used number input to take input

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,13 +6,20 @@ model = joblib.load('iris_model.pkl')
 
 st.title('Iris Flower Classification')
 
-sepal_length = st.slider('Sepal Length (cm)',4.0,8.0)
-sepal_width = st.slider("Sepal Width (cm)", 2.0, 5.0)
-petal_length = st.slider("Petal Length (cm)", 1.0, 7.0)
-petal_width = st.slider("Petal Width (cm)", 0.1, 2.5)
+# sepal_length = st.slider('Sepal Length (cm)',4.0,8.0)
+# sepal_width = st.slider("Sepal Width (cm)", 2.0, 5.0)
+# petal_length = st.slider("Petal Length (cm)", 1.0, 7.0)
+# petal_width = st.slider("Petal Width (cm)", 0.1, 2.5)
+
+sepal_length = st.number_input('Sepal Length (cm) 4.0,8.0')
+sepal_width = st.number_input('Sepal Width (cm)  2.0, 5.0')
+petal_length = st.number_input("Petal Length (cm) 1.0, 7.0")
+petal_width = st.number_input("Petal Width (cm) 0.1, 2.5")
 
 features = [[sepal_length, sepal_width, petal_length, petal_width]]
 
 if st.button("Predict"):
     prediction = model.predict(features)[0]
     st.success(f"Predicted Iris Class: {prediction}")
+
+st.write("Hello")
